81c3df13a07c7f6a0209a07816b790d91a0dd700
alex <alexander.kalinich@gmail.com>
2025-08-14 16:36:24 -0700

    adding example with overriding config

---
+++ tests/howto-override-config.spec.ts
@@ -0,0 +1,20 @@
+// @ts-check
+import {test} from '@playwright/test';
+
+//test.use({baseURL: 'https://some-other.com/'});
+
+test('test-1', async ({}, testInfo) => {
+    const bu = testInfo.project.use.baseURL
+    console.log('test-1 ' + bu)
+});
+
+test.describe('override', () => {
+        test.use({baseURL: 'https://example.com'});
+        test('test-2', async ({baseURL}) => {
+            console.log('test-2 ' + baseURL)
+        });
+    }
+)
+
+
+
