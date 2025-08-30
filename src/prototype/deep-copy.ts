f859f1c902fbe93c302998ec04b71d168be2bb01
alex <alexander.kalinich@gmail.com>
2025-08-30 15:13:15 -0700

    adding prototype example

---
+++ src/prototype/deep-copy.ts
@@ -0,0 +1,28 @@
+
+interface IProtoType {
+    clone(): this
+}
+
+class MyClass implements IProtoType {
+    field: number[]
+    constructor(field: number[]) {
+        this.field = field
+    }
+
+    clone() {
+        return JSON.parse(JSON.stringify(this))  // deep copy
+    }
+}
+
+// The Client
+
+const object1 = new MyClass([1,2,3,4])
+console.log(`object1: ${JSON.stringify(object1)}`)
+
+const object2 = object1.clone()
+console.log(`object2: ${JSON.stringify(object2)}`)
+
+object2.field[1] = 101
+console.log(`object2: ${JSON.stringify(object2)}`)
+// note object1 is not changed
+console.log(`object1: ${JSON.stringify(object1)}`)
