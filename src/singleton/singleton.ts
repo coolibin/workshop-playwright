137713cbdc0d83716622590f4520bbac3be4b9fd
alex <alexander.kalinich@gmail.com>
2025-08-30 15:39:03 -0700

    adding singleton example

---
+++ src/singleton/singleton.ts
@@ -0,0 +1,22 @@
+
+export class  Singleton {
+    static instance: Singleton
+    id: number
+
+    constructor(id: number) {
+        this.id = id
+        if (Singleton.instance) {
+            return Singleton.instance
+        }
+        Singleton.instance = this
+    }
+}
+
+// The Client
+
+const singleton1 = new Singleton(1)
+const singleton2 = new Singleton(2)
+
+console.log(singleton1 === singleton2)
+console.log(singleton1.id)
+console.log(singleton2.id)
